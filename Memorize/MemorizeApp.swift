7019a6ea0973c9bde5d5e3b2dfde6574a651e8eb
Valeriia Zakharova <valeriia.zakharova@adastra.one>
2023-07-24 10:18:03 +0200

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Valeriia Zakharova on 24.07.2023.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
